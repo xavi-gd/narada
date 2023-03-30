6098d37a97580ac7d4b79859c84e75d8612e7ac3
xavi-gd <80868670+xavi-gd@users.noreply.github.com>
2023-03-30 15:29:31 +0200

    python db

--- db_access.py
+++ db_access.py
@@ -43,4 +43,5 @@ flag = client.write_points(json_body)
 if not flag:
     print("ERROR in the client write.write_points")
 
-print(client.query('SELECT "duration" FROM "test"."autogen"."brushEvents" WHERE time > now() - 4d GROUP BY "user"'))
+#print(client.query('SELECT "duration" FROM "test"."autogen"."brushEvents" WHERE time > now() - 4d GROUP BY "user"'))
+print(client.query('SELECT * FROM "test"'))
